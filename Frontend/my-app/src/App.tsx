9150a4b61f3e5037eebcbb863fffe9b7e57d1893
zhdr64 <april64chen@gmail.com>
2022-08-10 15:01:09 +1200

    Created Frontend folder, step 0, 1, 2

---
+++ Frontend/my-app/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // Declare a new state variable, which we'll call "pokemonName"
+  const [pokemonName, setPokemonName] = useState("");
+
+  return (
+    <div>
+      <h1>
+        Pokemon Search
+      </h1>
+      
+      <div>
+        <label>Pokemon Name</label><br/>
+        <input type="text" id="pokemon-name" name="pokemon-name" onChange={e => setPokemonName(e.target.value)}/><br/>
+        <button onClick={search}>
+        Search
+        </button>
+      </div>
+
+      <p>
+        You have entered {pokemonName}
+      </p>
+    </div>
+  );
+
+  function search(){
+      alert("Search button has been clicked!");
+  }
+}
+
+export default App;

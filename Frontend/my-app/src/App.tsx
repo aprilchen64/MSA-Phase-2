1abb3332bebb5f6f98a50ef16d96b2ca0e35e856
zhdr64 <april64chen@gmail.com>
2022-08-10 20:34:47 +1200

    Different API and display information

--- Frontend/my-app/src/App.tsx
+++ Frontend/my-app/src/App.tsx
@@ -1,170 +1,70 @@
 import axios from "axios";
-import { Pokemon } from "pokenode-ts";
 import { useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
-import TextField from "@mui/material/TextField";
+import BackgroundImage from '../src/background.jpeg';
 import "./App.css";
 import { Box, Button, Grid, Paper, Skeleton } from "@mui/material";
 
 function App() {
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemonInfo, setPokemonInfo] = useState<null | undefined | Pokemon>(
-    undefined
-  );
-  const POKEMON_BASE_API_URL = "https://pokeapi.co/api/v2";
+  // Declare a new state variable, which we'll call "villager"
+  const [villager, setVillager] = useState<undefined | any>(undefined);
+  const [villagerName, setVillagerName] = useState<undefined | any>(undefined);
+
+  const BASE_API_URL = "https://acnhapi.com/v1/villagers/";
+
+  const styles = {
+    paperContainer: {
+        backgroundImage: `url(${BackgroundImage})`,
+        height: '100vh'
+    }
+};
+
+  
+
   return (
-    <div>
-      <div className="search-field">
-        <h1>Pokédex Search</h1>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <TextField
-            id="search-bar"
-            className="text"
-            value={pokemonName}
-            onChange={(prop) => {
-              setPokemonName(prop.target.value);
-            }}
-            label="Enter a Pokémon Name..."
-            variant="outlined"
-            placeholder="Search..."
-            size="medium"
-          />
-          <Button
-            onClick={() => {
-              search();
-            }}
-          >
-            <SearchIcon style={{ fill: "blue" }} />
-            Search
-          </Button>
-        </div>
+    <div style={styles.paperContainer}>
+      <h1>Animal Crossing Villagers Info</h1>
+
+      <div>
+        <Button variant="contained" onClick={search}>New Villager</Button>
       </div>
 
-      {pokemonInfo === undefined ? (
+      {villager === undefined ? (
         <div></div>
       ) : (
-        <div
-          id="pokemon-result"
-          style={{
-            maxWidth: "800px",
-            margin: "0 auto",
-            padding: "100px 10px 0px 10px",
-          }}
-        >
-          <Paper sx={{ backgroundColor: getBackColor(pokemonInfo) }}>
-            <Grid
-              container
-              direction="row"
-              spacing={5}
-              sx={{
-                justifyContent: "center",
-              }}
-            >
-              <Grid item>
-                <Box>
-                  {pokemonInfo === undefined || pokemonInfo === null ? (
-                    <h1> Pokemon not found</h1>
-                  ) : (
-                    <div>
-                      <h1>
-                        {pokemonInfo.name.charAt(0).toUpperCase() +
-                          pokemonInfo.name.slice(1)}
-                      </h1>
-                      <p>
-                        ID: {pokemonInfo.id}
-                        <br />
-                        Height: {pokemonInfo.height * 10} cm
-                        <br />
-                        Weight: {pokemonInfo.weight / 10} kg
-                        <br />
-                        Types: {getTypes()?.toString()}
-                        <br />
-                        Abilities: {getAbilities()?.toString()}
-                      </p>
-                    </div>
-                  )}
-                </Box>
-              </Grid>
-              <Grid item>
-                <Box>
-                  {pokemonInfo?.sprites.other.dream_world.front_default ? (
-                    <img
-                      height="300px"
-                      width="300px"
-                      alt={pokemonInfo.name}
-                      src={pokemonInfo.sprites.other.dream_world.front_default}
-                    ></img>
-                  ) : (
-                    <Skeleton width={300} height={300} />
-                  )}
-                </Box>
-              </Grid>
-            </Grid>
-          </Paper>
+        <div id="villager-result">
+          <h3>{villagerName}</h3>
+          <img src={villager["image_uri"]} />
+          <p>Birthday: {villager["birthday-string"]}</p>
+          <p>Species: {villager["species"]}</p>
+          <p>Gender: {villager["gender"]}</p>
+
+          <p>Catch phrase: {villager["catch-phrase"]}</p>
+          
+          <p>Hobby: {villager["hobby"]}</p>
+          <p>Personality: {villager["personality"]}</p>
+          <p>Saying: {villager["saying"]}</p>
+          
+
+          <p>Bubble colour: {villager["bubble-color"]}</p>
+          <p>Text colour: {villager["text-color"]}</p>
         </div>
+
+        
       )}
+
     </div>
   );
 
-  // Credit to codingsparkles for providing the color mapping
-  function getBackColor(poke: Pokemon | undefined | null) {
-    let backColor = "#EEE8AA";
-    if (poke === undefined || poke === null) {
-      return backColor;
-    }
-    const pokeTypes = poke.types.map((i) => i.type.name);
-    if (pokeTypes.includes("fire")) {
-      backColor = "#FEC5BB";
-    } else if (pokeTypes.includes("grass")) {
-      backColor = "#80FFDB";
-    } else if (pokeTypes.includes("water")) {
-      backColor = "#DFE7FD";
-    } else if (pokeTypes.includes("bug")) {
-      backColor = "#B0DEA3";
-    } else if (pokeTypes.includes("normal")) {
-      backColor = "#E0FFFF";
-    } else if (pokeTypes.includes("electric")) {
-      backColor = "#D8E2DC";
-    } else if (pokeTypes.includes("ground")) {
-      backColor = "#FAD2E1";
-    } else if (pokeTypes.includes("fairy")) {
-      backColor = "#FFF1E6";
-    } else if (pokeTypes.includes("ghost")) {
-      backColor = "#F8EDEB";
-    } else if (pokeTypes.includes("fighting")) {
-      backColor = "#F1FAEE";
-    } else if (pokeTypes.includes("rock")) {
-      backColor = "#A8DADC";
-    }
-    return backColor;
-  }
-
   function search() {
-    console.log(pokemonName);
-    if (pokemonName === undefined || pokemonName === "") {
-      return;
-    }
+    // Generate a random villager id
+    const id = Math.floor(Math.random() * 391) + 1;
 
-    axios
-      .get(POKEMON_BASE_API_URL + "/pokemon/" + pokemonName?.toLowerCase())
-      .then((res) => {
-        setPokemonInfo(res.data);
-      })
-      .catch(() => {
-        setPokemonInfo(null);
-      });
-  }
-
-  function getTypes() {
-    if (pokemonInfo !== undefined && pokemonInfo !== null) {
-      return pokemonInfo.types.map((item) => item.type.name);
-    }
-  }
-
-  function getAbilities() {
-    if (pokemonInfo !== undefined && pokemonInfo !== null) {
-      return pokemonInfo.abilities.map((ability) => ability.ability.name);
-    }
+    console.log(BASE_API_URL + id) // comment out when finished
+    axios.get(BASE_API_URL + id).then((res) => {
+      setVillager(res.data);
+      setVillagerName(res.data.name["name-USen"]);
+      console.log(res.data);
+    });
   }
 }
 
